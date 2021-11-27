84d4ee586fc6dd0a3c66f1c3a1585847faea65b0
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-11-27 23:52:10 +0100

    Improve install script

--- build/install.sh
+++ build/install.sh
@@ -10,24 +10,29 @@ set -o pipefail
 
 DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd -P)
 
-if ! command -v python &> /dev/null; then
+if command -v python3 &> /dev/null; then
+  PYTHON=python3
+elif command -v python &> /dev/null; then
+  PYTHON=python
+else
   echo "[install] Python is not installed."
   exit 1
 fi
 
-PYTHON_VERSION=$(python -V 2>&1 | grep -Eo '([0-9]+\.[0-9]+\.[0-9]+)$')
+PYTHON_VERSION=$($PYTHON -V 2>&1 | grep -Eo '([0-9]+\.[0-9]+\.[0-9]+)$')
 if [[ ${PYTHON_VERSION} < "3.7.0" ]]; then
   echo "[install] Python version 3.7.0 or higher is required."
   exit 1
 fi
 
-if ! command -v poetry &> /dev/null; then
+POETRY_HOME="${POETRY_HOME:=${HOME}/.local}"
+if ! command -v "$POETRY_HOME"/bin/poetry &> /dev/null; then
   echo "[install] Poetry is not installed. Begin download and install."
-  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | POETRY_HOME=$POETRY_HOME $PYTHON -
 fi
 
 echo "[install] Begin installing project."
-poetry install --extras "aioredis aiohttp" --no-interaction
+"$POETRY_HOME"/bin/poetry install --extras "aioredis aiohttp" --no-interaction
 
 cat <<EOF
 Project successfully installed.
