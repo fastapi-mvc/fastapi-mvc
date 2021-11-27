81d9cd89fc520748fcecfd49a2fc5322158c5410
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-11-27 15:17:44 +0100

    Refactor make venv target to make install with using poetry

--- build/install.sh
+++ build/install.sh
@@ -11,30 +11,28 @@ set -o pipefail
 DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd -P)
 
 if ! command -v python &> /dev/null; then
-  echo "python is not installed"
-  exit 1
-fi
-
-if ! command -v virtualenv &> /dev/null; then
-  echo "virtualenv is not installed"
+  echo "[install] Python is not installed."
   exit 1
 fi
 
 PYTHON_VERSION=$(python -V 2>&1 | grep -Eo '([0-9]+\.[0-9]+\.[0-9]+)$')
 if [[ ${PYTHON_VERSION} < "3.7.0" ]]; then
-  echo "Please upgrade python to 3.7.0 or higher"
+  echo "[install] Python version 3.7.0 or higher is required."
   exit 1
 fi
 
+if ! command -v poetry &> /dev/null; then
+  echo "[install] Poetry is not installed. Begin download and install."
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+fi
 
-echo "[venv] creating virtualenv"
-virtualenv --python=python venv
-echo "[venv] installing project"
-venv/bin/pip install .
+echo "[install] Begin installing project."
+poetry install --extras "aioredis aiohttp" --no-dev
 
 cat <<EOF
-Project successfully installed in virtualenv
-To activate virtualenv run from project root: $ source venv/bin/activate
-Now you should access CLI binary: $ fastapi --help
+Project successfully installed.
+To activate virtualenv run: $ poetry shell
+Now you should access CLI script: $ fastapi --help
+Alternatively you can access CLI script via poetry run: $ poetry run fastapi --help
 To deactivate virtualenv simply type: $ deactivate
 EOF
