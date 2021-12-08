5df71188648a0e47914ce4f1222a7fb12e9d2361
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-12-08 23:52:05 +0100

    Add configuration from env with default values

--- fastapi_mvc_template/app/config/application.py
+++ fastapi_mvc_template/app/config/application.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 """Application configuration."""
+import os
+
 from fastapi_mvc_template.version import __version__
 
 
 # FastAPI logging level
-DEBUG = True
+DEBUG = os.getenv('FASTAPI_DEBUG', True)
 # FastAPI project name
 PROJECT_NAME = "fastapi_mvc_template"
 VERSION = __version__
+# Whether or not to use Redis
+USE_REDIS = os.getenv('FASTAPI_USE_REDIS', True)
 # All your additional application configuration should go either here or in
 # separate file in this submodule.
