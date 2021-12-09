6c43fc959e1f65df312edd653021e112b6d7388b
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-12-09 16:41:22 +0100

    Implement Redis connection validation, refactor import paths

--- fastapi_mvc_template/app/controllers/api/v1/ready.py
+++ fastapi_mvc_template/app/controllers/api/v1/ready.py
@@ -3,7 +3,10 @@
 import logging
 
 from fastapi import APIRouter
-from fastapi_mvc_template.app.models.ready import ReadyResponse
+from fastapi_mvc_template.config import USE_REDIS
+from fastapi_mvc_template.app.utils import RedisClient
+from fastapi_mvc_template.app.models import ReadyResponse, ErrorResponse
+from fastapi_mvc_template.app.exceptions import HTTPException
 
 router = APIRouter()
 log = logging.getLogger(__name__)
@@ -15,18 +18,36 @@ log = logging.getLogger(__name__)
     response_model=ReadyResponse,
     summary="Simple health check.",
     status_code=200,
+    responses={502: {"model": ErrorResponse}}
 )
 async def readiness_check():
     """Run basic application health check.
 
-    If application is up and running then this endpoint will return simple
-    response with status ok. Otherwise app will be unavailable or an internal
-    server error will be returned.
+    If the application is up and running then this endpoint will return simple
+    response with status ok. Moreover, if it has Redis enabled then connection
+    to it will be tested. If Redis ping fails, then this endpoint will return
+    502 HTTP error.
     \f
 
     Returns:
         response (ReadyResponse): ReadyResponse model object instance.
 
+    Raises:
+        HTTPException: If applications has enabled Redis and can not connect
+            to it. NOTE! This is the custom exception, not to be mistaken with
+            FastAPI.HTTPException class.
+
     """
     log.info("Started GET /ready")
+
+    if USE_REDIS and not await RedisClient.ping():
+        log.error("Could not connect to Redis")
+        raise HTTPException(
+            status_code=502,
+            content=ErrorResponse(
+                code=502,
+                message="Could not connect to Redis"
+            ).dict(exclude_none=True)
+        )
+
     return ReadyResponse(status="ok")
