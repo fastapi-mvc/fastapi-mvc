e6b1c5c88eb2c8142a82d42605c96ee6822fd725
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-12-09 16:30:56 +0100

    Improve imports paths

--- fastapi_mvc_template/config/__init__.py
+++ fastapi_mvc_template/config/__init__.py
@@ -1,2 +1,25 @@
 # -*- coding: utf-8 -*-
 """FastAPI MVC template."""
+from .application import DEBUG, PROJECT_NAME, VERSION, USE_REDIS
+from .redis import (
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_USERNAME,
+    REDIS_PASSWORD,
+    REDIS_USE_SENTINEL,
+)
+from .router import router
+
+
+__all__ = (
+    DEBUG,
+    PROJECT_NAME,
+    VERSION,
+    USE_REDIS,
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_USERNAME,
+    REDIS_PASSWORD,
+    REDIS_USE_SENTINEL,
+    router
+)
