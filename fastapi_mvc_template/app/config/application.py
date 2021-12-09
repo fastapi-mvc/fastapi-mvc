e47351d2c9e9d758e24a2f768e603fbe5fbb4d1c
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-12-09 16:22:59 +0100

    Minor formatting change

--- fastapi_mvc_template/app/config/application.py
+++ fastapi_mvc_template/app/config/application.py
@@ -5,12 +5,12 @@ import os
 from fastapi_mvc_template.version import __version__
 
 
-# FastAPI logging level
-DEBUG = os.getenv('FASTAPI_DEBUG', True)
-# FastAPI project name
+# FastAPI logging level. You should disable this for production.
+DEBUG = os.getenv("FASTAPI_DEBUG", True)
+# FastAPI project name.
 PROJECT_NAME = "fastapi_mvc_template"
 VERSION = __version__
-# Whether or not to use Redis
-USE_REDIS = os.getenv('FASTAPI_USE_REDIS', True)
+# Whether or not to use Redis.
+USE_REDIS = os.getenv("FASTAPI_USE_REDIS", True)
 # All your additional application configuration should go either here or in
 # separate file in this submodule.
--- fastapi_mvc_template/app/config/redis.py
+++ fastapi_mvc_template/app/config/redis.py
@@ -3,9 +3,9 @@
 import os
 
 
-REDIS_HOST = os.getenv('FASTAPI_REDIS_HOST', 'localhost')
-REDIS_PORT = int(os.getenv('FASTAPI_REDIS_PORT', 6379))
-REDIS_USERNAME = os.getenv('FASTAPI_REDIS_USERNAME', None)
-REDIS_PASSWORD = os.getenv('FASTAPI_REDIS_PASSWORD', None)
+REDIS_HOST = os.getenv("FASTAPI_REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("FASTAPI_REDIS_PORT", 6379))
+REDIS_USERNAME = os.getenv("FASTAPI_REDIS_USERNAME", None)
+REDIS_PASSWORD = os.getenv("FASTAPI_REDIS_PASSWORD", None)
 # If provided above Redis config is for Sentinel.
-REDIS_USE_SENTINEL = bool(os.getenv('FASTAPI_REDIS_USE_SENTINEL', False))
+REDIS_USE_SENTINEL = bool(os.getenv("FASTAPI_REDIS_USE_SENTINEL", False))
