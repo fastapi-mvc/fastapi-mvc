78b91b81328c90493be9b2a169a52e1b6fae98db
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-01-07 20:59:57 +0100

    Implement command logic

--- fastapi_mvc/cli/commands/new.py
+++ fastapi_mvc/cli/commands/new.py
@@ -11,9 +11,9 @@ from cookiecutter.main import cookiecutter
 
 @click.command()
 @click.argument(
-    "APP_NAME",
+    "APP_PATH",
     nargs=1,
-    type=click.STRING,
+    type=click.Path(exists=False),
     required=True,
 )
 @click.option(
@@ -47,27 +47,63 @@ from cookiecutter.main import cookiecutter
     is_flag=True,
 )
 @click.option(
+    "-C",
+    "--skip-codecov",
+    help="Skip codecov in GitHub actions.",
+    is_flag=True,
+)
+@click.option(
+    "-I",
+    "--skip-install",
+    help="Dont run make install",
+    is_flag=True,
+)
+@click.option(
     "--license",
     help="Choose license.",
-    type=click.Choice(["MIT", "BSD2", "BSD3", "ISC", "Apache"]),
+    type=click.Choice(
+        [
+            "MIT",
+            "BSD2",
+            "BSD3",
+            "ISC",
+            "Apache2.0",
+            "LGPLv3+",
+            "LGPLv3",
+            "LGPLv2+",
+            "LGPLv2",
+            "no",
+        ]
+    ),
+    default="MIT",
+    show_default=True,
+    envvar="LICENSE",
 )
 @click.option(
     "--repo-url",
     help="Repository url.",
     type=click.STRING,
+    envvar="REPO_URL",
+    default="https://your.repo.url.here",
 )
-def new(app_name, **options):
+def new(app_path, **options):
     """Create a new FastAPI application.
 
     The 'fastapi-mvc new' command creates a new FastAPI application with a
-    default directory structure and configuration at the current path.
+    default directory structure and configuration at the path you specify.
     \f
 
     Args:
-        app_name(str): CLI command argument - new application name.
+        app_path(str): CLI command argument - new application path.
         options(dict): CLI command options.
 
     """
+    app_name = os.path.basename(app_path)
+    output_dir = os.path.dirname(app_path)
+
+    if not output_dir:
+        output_dir = "."
+
     template_dir = os.path.abspath(
         os.path.join(
             os.path.abspath(__file__),
@@ -76,19 +112,20 @@ def new(app_name, **options):
     )
 
     try:
-        user = (
-            subprocess.check_output(["git", "config", "--get", "user.name"])
-            .decode("utf-8")
-            .strip()
+        author = subprocess.check_output(
+            ["git", "config", "--get", "user.name"]
         )
-        email = (
-            subprocess.check_output(["git", "config", "--get", "user.email"])
-            .decode("utf-8")
-            .strip()
-        )
+        author = author.decode("utf-8").strip()
     except subprocess.CalledProcessError:
-        user = None
-        email = None
+        author = "John Doe"
+
+    try:
+        email = subprocess.check_output(
+            ["git", "config", "--get", "user.email"]
+        )
+        email = email.decode("utf-8").strip()
+    except subprocess.CalledProcessError:
+        email = "example@email.com"
 
     context = {
         "project_name": app_name,
@@ -97,7 +134,8 @@ def new(app_name, **options):
         "github_actions": "no" if options["skip_actions"] else "yes",
         "vagrantfile": "no" if options["skip_vagrantfile"] else "yes",
         "helm": "no" if options["skip_helm"] else "yes",
-        "author": user,
+        "codecov": "no" if options["skip_codecov"] else "yes",
+        "author": author,
         "email": email,
         "repo_url": options["repo_url"],
         "year": datetime.today().year,
@@ -108,13 +146,11 @@ def new(app_name, **options):
             template_dir,
             extra_context=context,
             no_input=True,
+            output_dir=output_dir,
         )
     except OutputDirExistsException as ex:
         click.echo(ex)
         sys.exit(1)
 
-    click.echo(
-        "FastAPI {app_name} project created successfully".format(
-            app_name=app_name
-        )
-    )
+    if not options["skip_install"]:
+        subprocess.run(["make", "install"], cwd=app_path)
