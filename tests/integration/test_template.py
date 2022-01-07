1d502fc4449a9dafce4c645d9c03dc650d24d18a
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-01-07 12:19:00 +0100

    Remove print

--- tests/integration/test_template.py
+++ tests/integration/test_template.py
@@ -7,8 +7,7 @@ def test_cookiecutter_template(cookies):
             os.path.abspath(__file__),
             "../../../fastapi_mvc/template",
         )
-    )
-    print(template_dir)
+    )ds
 
     result = cookies.bake(
         template=template_dir,
