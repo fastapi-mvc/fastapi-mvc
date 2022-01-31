ee364d9204a0974b96a199265384af38efa2f26c
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-01-31 19:03:54 +0100

    Extend unit tests

--- fastapi_mvc/template/{{cookiecutter.folder_name}}/tests/unit/test_wsgi.py
+++ fastapi_mvc/template/{{cookiecutter.folder_name}}/tests/unit/test_wsgi.py
@@ -2,24 +2,53 @@ import sys
 import os
 
 import mock
+import pytest
 from {{cookiecutter.package_name}}.wsgi import run_wsgi
 
 
+default_config = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "../../{{cookiecutter.package_name}}/config/gunicorn.conf.py"
+    )
+)
+
+
 @mock.patch("{{cookiecutter.package_name}}.wsgi.ApplicationLoader")
-def test_run_wsgi(loader_mock):
-    run_wsgi("localhost", "5555", "2")
+@pytest.mark.parametrize("kwargs, expected", [
+    (
+        {"host": "localhost", "port": "5555", "workers": "2"},
+        [
+            "--gunicorn",
+            "-c",
+            default_config,
+            "-w",
+            "2",
+            "-b localhost:5555",
+            "{{cookiecutter.package_name}}.app.asgi:application"
+        ]
+    ),
+    (
+        {"host": "0.0.0.0", "port": "80", "workers": "2", "daemon": True, "config": "/custom/gunicorn.conf.py", "env": ("FOO=BAR", "USE_FORCE=True"), "pid": "/path/to/file.pid"},
+        [
+            "--gunicorn",
+            "-c",
+            "/custom/gunicorn.conf.py",
+            "-w",
+            "2",
+            "-b 0.0.0.0:80",
+            "--daemon",
+            "--env",
+            "FOO=BAR",
+            "--env",
+            "USE_FORCE=True",
+            "--pid",
+            "/path/to/file.pid",
+            "{{cookiecutter.package_name}}.app.asgi:application"
+        ]
+    ),
+])
+def test_run_wsgi(loader_mock, kwargs, expected):
+    run_wsgi(**kwargs)
     loader_mock.assert_called_once()
-    assert sys.argv == [
-        "--gunicorn",
-        "-c",
-        os.path.abspath(
-            os.path.join(
-                os.path.dirname(__file__),
-                "../../{{cookiecutter.package_name}}/config/gunicorn.conf.py"
-            )
-        ),
-        "-w",
-        "2",
-        "-b localhost:5555",
-        "{{cookiecutter.package_name}}.app.asgi:application"
-    ]
+    assert sys.argv == expected
