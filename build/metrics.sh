aa5f5a0ef670152c5b1ae62b3b04be460c2de787
Antoine Merino <3023499+Merinorus@users.noreply.github.com>
2022-04-06 14:56:50 +0200

    fix space

--- build/metrics.sh
+++ build/metrics.sh
@@ -17,6 +17,6 @@ echo "[metrics] Run fastapi-mvc PEP 257 checks."
 echo "[metrics] Run fastapi-mvc code complexity checks."
 "$POETRY_HOME"/bin/poetry run flake8 --select=C901 --tee --exclude fastapi_mvc/template --output-file=code_complexity.txt --count fastapi_mvc
 echo "[metrics] Run fastapi-mvc open TODO checks."
-"$POETRY_HOME"/bin/poetry run flake8 --select=T --tee  --exclude fastapi_mvc/template--output-file=todo_occurence.txt --statistics --count fastapi_mvc tests
+"$POETRY_HOME"/bin/poetry run flake8 --select=T --tee --exclude fastapi_mvc/template --output-file=todo_occurence.txt --statistics --count fastapi_mvc tests
 echo "[metrics] Run fastapi-mvc black checks."
 "$POETRY_HOME"/bin/poetry run black -l 80 --exclude fastapi_mvc/template --check fastapi_mvc
