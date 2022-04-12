e9c98b898d5aef64683d1e8e6b5e43f9d708b08b
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-04-12 23:58:04 +0200

    [skip ci] Implement custom class for generating programatically subcommands based on available generators

--- fastapi_mvc/cli/generate.py
+++ fastapi_mvc/cli/generate.py
@@ -1,33 +1,47 @@
 """FastAPI MVC CLI generate command implementation."""
+import os
+
 import click
+from fastapi_mvc.generators.loader import load_generators
 
 
-@click.group()
-@click.option(
-    "-f",
-    "--force",
-    help="Overwrite files that already exist",
-    is_flag=True,
-)
-@click.option(
-    "-s",
-    "--skip",
-    help="Skip files that already exist",
-    is_flag=True,
-)
-def generate(**options):
-    print(options)
+generators = load_generators()
 
 
-@generate.command()
-@click.argument(
-    "NAME",
-    nargs=1,
-)
-@click.argument(
-    "ENDPOINTS",
-    nargs=-1,
-    required=False,
-)
-def controller(**options):
+class MyCLI(click.MultiCommand):
+
+    def list_commands(self, ctx):
+        return generators.keys()
+
+    def get_command(self, ctx, name):
+        params = []
+        epilog = None
+        generator = generators[name]
+
+        for arg in generator.cli_arguments:
+            params.append(click.Argument(**arg))
+
+        for opt in generator.cli_options:
+            params.append(click.Option(**opt))
+
+        if os.path.isfile(generator.usage):
+            with open(generator.usage, "r") as f:
+                epilog = f.read()
+
+        return click.Command(
+            name,
+            params=params,
+            callback=invoke_generator,
+            epilog=epilog,
+        )
+
+
+@click.command(cls=MyCLI)
+def generate():
+    pass
+
+
+@click.pass_context
+def invoke_generator(ctx, **options):
+    print(ctx.command.name)
     print(options)
