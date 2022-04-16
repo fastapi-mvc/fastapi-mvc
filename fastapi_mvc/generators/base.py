1b56b6da1e394a6eef250abc818a62008145d58f
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-04-16 18:55:44 +0200

    [skip ci] Add docstrings

--- fastapi_mvc/generators/base.py
+++ fastapi_mvc/generators/base.py
@@ -1,9 +1,30 @@
+"""FastAPI MVC Generate abstract class implementation."""
 from abc import ABCMeta, abstractmethod
 import logging
 
 
 class Generator(object, metaclass=ABCMeta):
-    """Defines the common interface for all concrete generators."""
+    """Defines the common interface for all concrete generators.
+
+    Attributes:
+        name (str): (class attribute) A distinguishable generator name, that
+            will be used in fastapi-mvc generate CLI command
+        template (str): (class attribute)  Path to generator cookiecutter
+            template directory.
+        usage (str): (class attribute) Path to generator usage file.
+        cli_arguments (list[dict]): (class attribute) Click arguments as kwargs,
+            that will be available in dynamically generated command for this
+            generator.
+        cli_options (list[dict]): (class attribute) Click options as kwargs,
+            that will be available in dynamically generated command for this
+            generator.
+        _log (logging.Logger): Logger class object instance.
+        _parser (fastapi_mvc.parsers.IniParser): IniParser object instance
+            for current fastapi-mvc project.
+        _project_root (str): Path to current fastapi-mvc project root.
+        _context (dict): Cookiecutter template minimal required context.
+
+    """
 
     __slots__ = (
         "_log",
@@ -33,7 +54,14 @@ class Generator(object, metaclass=ABCMeta):
     ]
 
     def __init__(self, parser, project_root):
-        """Initialize Command base class object instance."""
+        """Initialize Generator base class object instance.
+
+        Args:
+            parser (fastapi_mvc.parsers.IniParser): IniParser object instance
+                for current fastapi-mvc project.
+            project_root(str): Path to current fastapi-mvc project root.
+
+        """
         self._log = logging.getLogger(self.__class__.__name__)
         self._parser = parser
         self._project_root = project_root
@@ -44,10 +72,20 @@ class Generator(object, metaclass=ABCMeta):
 
     @abstractmethod
     def new(self, **kwargs):
-        """New abstract method for all inheriting classes."""
+        """New abstract method for all inheriting classes.
+
+        Args:
+            **kwargs(dict): Abstract methods kwargs.
+
+        """
         pass
 
     @abstractmethod
     def destroy(self, **kwargs):
-        """Destroy abstract method for all inheriting classes."""
+        """Destroy abstract method for all inheriting classes.
+
+        Args:
+            **kwargs(dict): Abstract methods kwargs.
+
+        """
         pass
