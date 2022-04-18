c29aec6d4a67d7c88563ca62dfbf66c3322bcb19
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-04-18 23:39:56 +0200

    Update generator generator template

--- fastapi_mvc/generators/generator/template/{{cookiecutter.folder_name}}/lib/generators/{{cookiecutter.generator_name}}/__init__.py
+++ fastapi_mvc/generators/generator/template/{{cookiecutter.folder_name}}/lib/generators/{{cookiecutter.generator_name}}/__init__.py
@@ -1,5 +1,5 @@
 """Custom generator for fastapi-mvc."""
-from .generator import {{cookiecutter.class_name}}
+from .{{cookiecutter.generator_name}} import {{cookiecutter.class_name}}
 
 # NOTE! Do not edit this! Method for programmatically loading user generators
 # depends on having only one class in module __all__ attribute.
