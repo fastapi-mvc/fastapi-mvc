180d33f3aa5037dc52272cb2e0cb4402bdcc7544
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-04-21 15:30:40 +0200

    Update metrics script with new excludes

--- build/metrics.sh
+++ build/metrics.sh
@@ -11,12 +11,12 @@ set -o pipefail
 
 POETRY_HOME="${POETRY_HOME:=${HOME}/.poetry}"
 echo "[metrics] Run fastapi-mvc PEP 8 checks."
-"$POETRY_HOME"/bin/poetry run flake8 --select=E,W,I --max-line-length 80 --import-order-style pep8 --exclude .git,__pycache__,.eggs,*.egg,.pytest_cache,fastapi_mvc/version.py,fastapi_mvc/__init__.py,fastapi_mvc/template --tee --output-file=pep8_violations.txt --statistics --count fastapi_mvc
+"$POETRY_HOME"/bin/poetry run flake8 --select=E,W,I --max-line-length 80 --import-order-style pep8 --exclude .git,__pycache__,.eggs,*.egg,.pytest_cache,fastapi_mvc/version.py,fastapi_mvc/__init__.py,fastapi_mvc/template,fastapi_mvc/generators/**/template --tee --output-file=pep8_violations.txt --statistics --count fastapi_mvc
 echo "[metrics] Run fastapi-mvc PEP 257 checks."
-"$POETRY_HOME"/bin/poetry run flake8 --select=D --ignore D301 --tee --exclude fastapi_mvc/template --output-file=pep257_violations.txt --statistics --count fastapi_mvc
+"$POETRY_HOME"/bin/poetry run flake8 --select=D --ignore D301 --tee --exclude fastapi_mvc/template,fastapi_mvc/generators/**/template --output-file=pep257_violations.txt --statistics --count fastapi_mvc
 echo "[metrics] Run fastapi-mvc code complexity checks."
-"$POETRY_HOME"/bin/poetry run flake8 --select=C901 --tee --exclude fastapi_mvc/template --output-file=code_complexity.txt --count fastapi_mvc
+"$POETRY_HOME"/bin/poetry run flake8 --select=C901 --tee --exclude fastapi_mvc/template,fastapi_mvc/generators/**/template --output-file=code_complexity.txt --count fastapi_mvc
 echo "[metrics] Run fastapi-mvc open TODO checks."
-"$POETRY_HOME"/bin/poetry run flake8 --select=T --tee --exclude fastapi_mvc/template --output-file=todo_occurence.txt --statistics --count fastapi_mvc tests
+"$POETRY_HOME"/bin/poetry run flake8 --select=T --tee --exclude fastapi_mvc/template,fastapi_mvc/generators/**/template --output-file=todo_occurence.txt --statistics --count fastapi_mvc tests
 echo "[metrics] Run fastapi-mvc black checks."
-"$POETRY_HOME"/bin/poetry run black -l 80 --exclude fastapi_mvc/template --check fastapi_mvc
+"$POETRY_HOME"/bin/poetry run black -l 80 --verbose --extend-exclude "fastapi_mvc/template" --exclude "fastapi_mvc/generators/.*/template"  --check fastapi_mvc
