886576ed1e7b48fc29b27326c48e242244737872
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-05-03 23:38:51 +0200

    Refactor generator loader method into Borg class

--- fastapi_mvc/generators/__init__.py
+++ fastapi_mvc/generators/__init__.py
@@ -3,7 +3,6 @@ from fastapi_mvc.generators.base import Generator
 from fastapi_mvc.generators.project import ProjectGenerator
 from fastapi_mvc.generators.controller import ControllerGenerator
 from fastapi_mvc.generators.generator import GeneratorGenerator
-from fastapi_mvc.generators.loader import load_generators
 
 
 __all__ = (
@@ -11,5 +10,4 @@ __all__ = (
     ProjectGenerator,
     ControllerGenerator,
     GeneratorGenerator,
-    load_generators,
 )
