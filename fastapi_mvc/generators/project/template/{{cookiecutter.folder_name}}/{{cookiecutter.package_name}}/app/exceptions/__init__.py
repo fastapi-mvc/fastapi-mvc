4478dae61bfe24f10bd09f5fe041ad8a3492c73e
Radosław Szamszur <github@rsd.sh>
2022-09-18 13:45:04 +0200

    Remove project template from fastapi-mvc

--- fastapi_mvc/generators/project/template/{{cookiecutter.folder_name}}/{{cookiecutter.package_name}}/app/exceptions/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Application implementation - exceptions."""
-from {{cookiecutter.package_name}}.app.exceptions.http import (
-    HTTPException,
-    http_exception_handler,
-)
-
-
-__all__ = ("HTTPException", "http_exception_handler")
