42d890666149787b5a6fa27ef32cf6e0cff53213
Radosław Szamszur <github@rsd.sh>
2022-10-08 14:40:58 +0200

    Refactor new project generator to copier

--- fastapi_mvc/cli/new.py
+++ fastapi_mvc/cli/new.py
@@ -1,37 +1,151 @@
 """Command-line interface - new command."""
+import logging
+import os
+from datetime import datetime
+
 import click
-from fastapi_mvc import Borg
-from fastapi_mvc.cli.click_custom import GeneratorCommand
-from fastapi_mvc.commands import RunGenerator
-from fastapi_mvc.generators import ProjectGenerator
+from fastapi_mvc import Generator, VERSION
+from fastapi_mvc.utils import run_shell, get_git_user_info
 
 
+log = logging.getLogger(__name__)
+cmd_short_help = "Create a new FastAPI application."
+cmd_help = """\
+The 'fastapi-mvc new' command creates a new FastAPI application with a
+default directory structure and configuration at the path you specify.
+
+Default Project template used: https://github.com/fastapi-mvc/copier-project
+
+"""
+
+
+@click.command(
+    cls=Generator,
+    template="https://github.com/fastapi-mvc/copier-project.git",
+    vcs_ref="master",
+    help=cmd_help,
+    short_help=cmd_short_help,
+)
+@click.argument(
+    "APP_PATH",
+    nargs=1,
+    type=click.Path(exists=False),
+    required=True,
+)
+@click.option(
+    "-R",
+    "--skip-redis",
+    help="Skip Redis utility files.",
+    is_flag=True,
+)
+@click.option(
+    "-A",
+    "--skip-aiohttp",
+    help="Skip aiohttp utility files.",
+    is_flag=True,
+)
+@click.option(
+    "-H",
+    "--skip-helm",
+    help="Skip Helm chart files.",
+    is_flag=True,
+)
+@click.option(
+    "-G",
+    "--skip-actions",
+    help="Skip GitHub actions files.",
+    is_flag=True,
+)
+@click.option(
+    "--skip-install",
+    help="Do not run make install after project generation.",
+    is_flag=True,
+)
+@click.option(
+    "-N",
+    "--skip-nix",
+    help="Skip nix expression files.",
+    is_flag=True,
+)
+@click.option(
+    "--license",
+    help="Choose license.",
+    type=click.Choice(
+        [
+            "MIT",
+            "BSD2",
+            "BSD3",
+            "ISC",
+            "Apache2.0",
+            "LGPLv3+",
+            "LGPLv3",
+            "LGPLv2+",
+            "LGPLv2",
+            "no",
+        ]
+    ),
+    default="MIT",
+    show_default=True,
+)
+@click.option(
+    "--repo-url",
+    help="New project repository url.",
+    type=click.STRING,
+    envvar="REPO_URL",
+    default="https://your.repo.url.here",
+)
+@click.option(
+    "--no-interaction",
+    help="Do not ask any interactive question.",
+    is_flag=True,
+)
+@click.option(
+    "--copier-repo",
+    help="Overrides fastapi-mvc copier-project repository.",
+    type=click.STRING,
+)
+@click.option(
+    "--copier-version",
+    help="The branch, tag or commit ID to checkout after clone.",
+    type=click.STRING,
+)
 @click.pass_context
-def invoke_generator(ctx, **params):
-    """Invoke project generator.
+def new(ctx, app_path, **options):
+    if options["copier_repo"]:
+        ctx.command.template = options["copier_repo"]
+    if options["copier_version"]:
+        ctx.command.vcs_ref = options["copier_version"]
 
-    Args:
-        ctx (click.Context): Click Context class object instance.
-        params (typing.Dict[str, typing.Any]): Map of command option and
-            argument names to their parsed values.
+    author, email = get_git_user_info()
+    data = {
+        "project_name": os.path.basename(app_path),
+        "author": author,
+        "email": email,
+        "copyright_date": datetime.today().year,
+        "fastapi_mvc_version": VERSION,
+        "nix": not options["skip_nix"],
+        "redis": not options["skip_redis"],
+        "aiohttp": not options["skip_aiohttp"],
+        "github_actions": not options["skip_actions"],
+        "helm": not options["skip_helm"],
+        "license": options["license"],
+        "repo_url": options["repo_url"],
+        "container_image_name": os.path.basename(app_path),
+        "chart_name": os.path.basename(app_path),
+        "script_name": os.path.basename(app_path),
+        "project_description": "This project was generated with fastapi-mvc.",
+        "version": "0.1.0",
+    }
 
-    """
-    borg = Borg()
+    if options["no_interaction"]:
+        ctx.command.run_auto(data=data)
+    else:
+        ctx.command.run_auto(user_defaults=data)
 
-    generator = ctx.command.generator_cls()
-
-    borg.enqueue(RunGenerator(generator=generator, options=params))
-    borg.execute()
-
-
-def get_new_cmd():
-    """Return command-line interface new command.
-
-    Returns:
-        GeneratorCommand: Class object instance.
-
-    """
-    return GeneratorCommand(
-        generator_cls=ProjectGenerator,
-        callback=invoke_generator,
-    )
+    if not options["skip_install"]:
+        ctx.command.copier_printf(
+            action="run",
+            msg="make install",
+            style="OK",
+        )
+        run_shell(cmd=["make", "install"], cwd=app_path)
