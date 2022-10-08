8320f61bc0397596bbfd8c061a196704738cebbb
Radosław Szamszur <github@rsd.sh>
2022-10-08 14:15:42 +0200

    Refactor ShellUtils classmethods to standalone methods

--- fastapi_mvc/utils/__init__.py
+++ fastapi_mvc/utils/__init__.py
@@ -4,8 +4,8 @@ The ``fastapi-mvc.utils`` submodule implements generic utilities for the need of
 the package.
 
 """
-from fastapi_mvc.utils.shell import ShellUtils
+from fastapi_mvc.utils.shell import run_shell, get_git_user_info
 from fastapi_mvc.utils.excepthook import global_except_hook
 
 
-__all__ = ("ShellUtils", "global_except_hook")
+__all__ = ("run_shell", "get_git_user_info", "global_except_hook")
