99cfe17416c677d850ee3cd529c00561417db8ce
Radosław Szamszur <github@rsd.sh>
2022-10-09 20:11:59 +0200

    Add intergartion tests

---
+++ tests/integration/test_generator_generator.py
@@ -0,0 +1,37 @@
+import os
+
+from fastapi_mvc.cli.new import new
+from fastapi_mvc.generators import generator
+
+
+def assert_paths(paths, condition):
+    for path in paths:
+        assert condition(path)
+
+
+def test_generator(cli_runner, fake_project, monkeypatch):
+    monkeypatch.chdir(fake_project)
+    result = cli_runner.invoke(
+        generator,
+        ["foo-bar"],
+    )
+    assert result.exit_code == 0
+
+    paths = [
+        "lib/generators/foo_bar/foo_bar.py",
+        "lib/generators/foo_bar/__init__.py",
+        "lib/generators/foo_bar/.envrc",
+        "lib/generators/foo_bar/.gitignore",
+        "lib/generators/foo_bar/CHANGELOG.md",
+        "lib/generators/foo_bar/README.md",
+        "lib/generators/foo_bar/copier.yml",
+        "lib/generators/foo_bar/poetry.lock",
+        "lib/generators/foo_bar/pyproject.toml",
+        "lib/generators/foo_bar/update.sh",
+        "lib/generators/foo_bar/default.nix",
+        "lib/generators/foo_bar/shell.nix",
+        "lib/generators/foo_bar/.generator.yml",
+        "lib/generators/foo_bar/template/{{package_name}}/hello_world.py.jinja",
+    ]
+
+    assert_paths(paths, condition=lambda x: os.path.isfile(x))
