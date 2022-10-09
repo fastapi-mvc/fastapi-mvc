9588411b847b07fb0d270ac65fca59f1b08e8ffd
Radosław Szamszur <github@rsd.sh>
2022-10-09 21:59:38 +0200

    Update version import

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-from fastapi_mvc import __version__
+from fastapi_mvc import VERSION
 from pallets_sphinx_themes import ProjectLink
 
 # Project --------------------------------------------------------------
@@ -11,7 +11,7 @@ from pallets_sphinx_themes import ProjectLink
 project = "fastapi-mvc"
 copyright = "2022, Radosław Szamszur"
 author = "Radosław Szamszur"
-release = __version__
+release = VERSION
 
 # General --------------------------------------------------------------
 
