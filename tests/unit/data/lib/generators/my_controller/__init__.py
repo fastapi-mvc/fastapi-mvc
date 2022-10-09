84210b949bcb2540ea0da64f279206616d2ae92b
Radosław Szamszur <github@rsd.sh>
2022-10-09 13:33:30 +0200

    Remove old generators examples for unit tests

--- tests/unit/data/lib/generators/my_controller/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Custom generator for fastapi-mvc."""
-from .my_controller import MyControllerGenerator
-
-# NOTE! Do not edit this! Method for programmatically loading user generators
-# depends on having only one class in module `generator_class` attribute.
-generator_class = MyControllerGenerator
