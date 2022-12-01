4a761b13764cb0b00ca5c2248c490969ad29c8a4
Radosław Szamszur <github@rsd.sh>
2022-12-01 18:12:52 +0100

    Update poetry2nix, update overrides

--- overlay.nix
+++ overlay.nix
@@ -1,10 +1,10 @@
 final: prev:
 let
   src = final.fetchFromGitHub {
-    owner = "nix-community";
+    owner = "SemMulder";
     repo = "poetry2nix";
-    rev = "1.31.0";
-    sha256 = "06psv5mc7xg31bvjpg030mwnk0sv90cj5bvgsdmcwicifpl3k3yj";
+    rev = "fix-750";
+    sha256 = "0z0gzw7ic9mn846rz968r7v0z44pklzpn6pv4kfzg63xxr4wxzyr";
   };
   p2n = import "${src.out}/default.nix" { pkgs = final; poetry = final.poetry; };
 in
@@ -15,16 +15,18 @@ in
     # py-final & py-prev refers to python packages
     defaultPoetryOverrides = p2n-prev.defaultPoetryOverrides.extend (py-final: py-prev: {
 
-      mdit-py-plugins = py-prev.mdit-py-plugins.overridePythonAttrs (old: {
-        buildInputs = old.buildInputs or [ ] ++ [ py-final.flit-core ];
-      });
-
-      idna = py-prev.idna.overridePythonAttrs (old: {
-        buildInputs = old.buildInputs or [ ] ++ [ py-final.flit-core ];
+      flake8-todo = py-prev.flake8-todo.overridePythonAttrs (old: {
+        buildInputs = old.buildInputs or [ ] ++ [ py-final.setuptools ];
       });
 
       sphinx = py-prev.sphinx.overridePythonAttrs (old: {
         buildInputs = old.buildInputs or [ ] ++ [ py-final.flit-core ];
+      });
+
+      pyyaml-include = py-prev.pyyaml-include.overridePythonAttrs (old: {
+        postPatch = ''
+          substituteInPlace setup.py --replace 'setup()' 'setup(version="${old.version}")'
+        '';
       });
 
     });
