63ae5afe8fb198474196c24b0c80311b979ec2e8
Radosław Szamszur <github@rsd.sh>
2023-01-08 15:36:39 +0100

    Extend integration tests conftest fixtures

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,13 +1,37 @@
 import os
+import copy
 
 import pytest
 from click.testing import CliRunner
 from fastapi_mvc.cli.new import new
+from fastapi_mvc.cli.update import update
+
+
+COPIER_PROJECT_RELEASES = {
+    "0.1.0",
+    "0.1.1",
+    "0.2.0",
+    "0.3.0",
+}
 
 
 def assert_paths(paths, condition):
     for path in paths:
         assert condition(path)
+
+
+@pytest.fixture
+def new_copy():
+    new_copy = copy.deepcopy(new)
+    yield new_copy
+    del new_copy
+
+
+@pytest.fixture
+def update_copy():
+    update_copy = copy.deepcopy(update)
+    yield update_copy
+    del update_copy
 
 
 @pytest.fixture(scope="session")
@@ -25,6 +49,37 @@ def cli_runner():
 def default_project(tmp_path_factory, cli_runner):
     tmp_dir = tmp_path_factory.mktemp("projects")
     root_dir = tmp_dir / "default-project"
-    cli_runner.invoke(new, ["--skip-install", "--no-interaction", str(root_dir)])
+    result = cli_runner.invoke(
+        copy.deepcopy(new),
+        ["--skip-install", "--no-interaction", f"{root_dir}"],
+    )
+    # make sure project was generated successfully
+    assert result.exit_code == 0
 
     return root_dir
+
+
+@pytest.fixture(scope="session")
+def reference_projects(tmp_path_factory, cli_runner):
+    tmp_dir = tmp_path_factory.mktemp("releases")
+    projects = dict()
+
+    for release in COPIER_PROJECT_RELEASES:
+        project_dir = tmp_dir / release / "update-test"
+        project_dir.mkdir(parents=True)
+        result = cli_runner.invoke(
+            copy.deepcopy(new),
+            [
+                "--skip-install",
+                "--no-interaction",
+                "--use-version",
+                release,
+                str(project_dir),
+            ]
+        )
+        # make sure project was generated successfully
+        assert result.exit_code == 0
+
+        projects[release] = project_dir
+
+    return projects
